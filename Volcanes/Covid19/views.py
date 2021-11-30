53a01888b086bf10e43831a2ebcce28d7a1562b4
sgharringtona <seth.harrington@ucr.ac.cr>
2021-11-30 16:08:12 -0600

    Se empezo el Markdown Tambien se cambio de lugar las bases de datos para poder importarlos con mayor facilidad

--- Volcanes/Covid19/views.py
+++ Volcanes/Covid19/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from Covid19.analysis import Analysis
 # Create your views here.
+'''fecha = ['14/07/2021','20/07/2021'] #fecha para pruebas
+obj = Analysis('./07_20_21_CSV_ACTIVOS_UTF8.csv')
 
-
+obj.x
+obj.y
+obj.canton('Sarapiquí', fecha )
+#obj.canton('San Carlos', fecha )
+obj.Acotar(fecha,['Acosta','Alajuelita'])'''
 def CovidDashboard(request):
     plantilla = "Covid19/reportes.html"
     return render(request, plantilla)
 
 def SanJoseReport(request):
+    datosx = [125, 100, 3, 25, 2, 3]
     plantilla1 = "Covid19/sanjose.html"
-    return render(request, plantilla1)
+    return render(request, plantilla1, {"datosx":datosx})
